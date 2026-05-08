8979403ac7d9d6123193636ede0761067f165a32
Ayush <ayushprasad481@gmail.com>
2026-05-08 18:23:15 +0530

    Implement resume upload and PDF text extraction

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import authRoutes from "./auth.js";
 import jobRoutes from "./job.js";
+import candidateRoutes from "./candidate.js";
 import {authenticateToken} from "../middleware/jwt.js";
 
 const routes = (app) => {
     app.use("/api/auth", authRoutes);
     app.use("/api/job", authenticateToken, jobRoutes);
+    app.use("/api/candidate", authenticateToken, candidateRoutes);
 };
 
 export default routes;
